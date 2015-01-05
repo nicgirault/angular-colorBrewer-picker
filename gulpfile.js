bd068678a16f33de6de3249e0fe1f8b3f8f7050d
Nico <nic.girault@gmail.com>
2015-01-05 19:28:00 +0100

    rename distribution files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less')
 var coffee = require('gulp-coffee');
 var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 gulp.task('coffee', function(){
   gulp.src('src/**/*.coffee')
@@ -12,12 +13,14 @@ gulp.task('coffee', function(){
 gulp.task('browserify', ['coffee'], function() {
   gulp.src('build/colorBrewer.module.js')
   .pipe(browserify())
+  .pipe(rename('colorBrewer.js'))
   .pipe(gulp.dest('dist'))
 });
 
 gulp.task('less', function(){
   gulp.src('src/style.less')
   .pipe(less())
+  .pipe(rename('colorBrewer.css'))
   .pipe(gulp.dest('dist'))
 });
 
